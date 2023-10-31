2ae09713d1e1fdf76491154cf1bddcd55101206c
Pritam Halder <halderpritamhalder1@gmail.com>
2023-10-31 10:19:59 +0530

    today initial code

--- auth/src/App.jsx
+++ auth/src/App.jsx
@@ -1,26 +1,16 @@
 
 // src/App.js
-import React, { useContext } from 'react';
-import { Routes, Route, Navigate } from 'react-router-dom';
-import { Container, Typography, Box } from '@mui/material';
-import { PermissionProvider, PermissionContext } from './Context/PermissionContext';
+import React from 'react';
 import LoginPage from './Pages/LoginPage';
 import TicTacPage from './Pages/TicTacPage';
 import CsvPage from './Pages/CsvPage';
 import PermissionPage from './Pages/PermissionPage';
-import { useState } from 'react'
 import {createBrowserRouter,RouterProvider} from "react-router-dom"
 import Layout from './Layout/Layout'
 import UsersPage from './Pages/UsersPage'
+import PrivateGameRoute from './Components/PrivateGameRoute';
+import PrivateCsvEditRoute from './Components/PrivateCsvEditRoute';
 
-function PrivateGameRoute({children}) {
-  const { gameAuth } = useContext(PermissionContext);
-  return children
-}
-function PrivateCsvEditRoute({children}) {
-  const { csvAuth } = useContext(PermissionContext);
-  return children
-}
 
 
 const router = createBrowserRouter([
@@ -28,7 +18,7 @@ const router = createBrowserRouter([
     element:<Layout/>,
     children:[
       {
-        path:"/",
+        path:"/csv",
         element:<PrivateCsvEditRoute>
                  <CsvPage/>
                 </PrivateCsvEditRoute>
@@ -52,7 +42,7 @@ const router = createBrowserRouter([
    ],
   },
   {
-    path:"/login",
+    path:"/",
     element:<LoginPage/>,
   },
 ])
